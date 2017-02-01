3d3fc05551bd92048df3936477aa4569f063e970
Yasser Hussain <yasserhussain1110@gmail.com>
2017-02-02 00:43:38 +0530

    Added random color

--- client/lib/StockChartLib.js
+++ client/lib/StockChartLib.js
@@ -1,9 +1,13 @@
 import {Theme, Options} from './StockChartConfig';
 import Highcharts from 'highcharts/highstock';
+import randomColor from 'randomcolor';
 Highcharts.setOptions(Theme);
 
 const updateStockChart = (allSeriesOptions) => {
-  Options.series = allSeriesOptions;
+  Options.series = allSeriesOptions.map(s => {
+    s.color = randomColor();
+    return s;
+  });
   Highcharts.stockChart('chart', Options);
 };
 
